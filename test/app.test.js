a37c60367a668c775aa467739572267025dd02ea
Rae Schneider <rannschneid@gmail.com>
2020-12-05 16:05:26 -0700

    refactored server, added tests

---
+++ test/app.test.js
@@ -0,0 +1,58 @@
+const supertest = require('supertest');
+const app = require('../app');
+const { expect } = require('chai');
+
+describe('GET /apps', () => {
+  it('should return an array of apps', () => {
+    return supertest(app)
+      .get('/apps')
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array')
+        expect(res.body).to.have.lengthOf.at.least(1)
+        const app = res.body[0]
+        expect(app).to.include.all.keys(
+          'App', 'Rating', 'Genres'
+        )
+      })
+  })
+
+  it('should be 400 if sort is incorrect', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'MISTAKE' })
+      .expect(400, 'Sort must be one of app name or rating');
+  });
+
+  it('should sort by app', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ sort: 'App' })
+      .expect(200)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        let sorted = true;
+
+        let i = 0;
+        // iterate once less than the length of the array
+        // because we're comparing 2 items in the array at a time
+        while (i < res.body.length - 1) {
+          // compare App at `i` with next App at `i + 1`
+          const appAtI = res.body[i];
+          const appAtIPlus1 = res.body[i + 1];
+          // if the next App is less than the App at i,
+          if (appAtIPlus1.title < appAtI.app) {
+            // the books were not sorted correctly
+            sorted = false;
+            break; // exit the loop
+          }
+          i++;
+        }
+        expect(sorted).to.be.true;
+      });
+  });
+
+
+});
